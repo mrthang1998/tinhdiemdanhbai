e86efdd1394ff4fd083e863164172cef050e3e6b
Thang <mrthang1998142@gmail.com>
2023-03-30 17:30:59 +0700

    version 1

---
+++ index.js
@@ -0,0 +1,63 @@
+let point_player_1 = 0;
+let point_player_2 = 0;
+let point_player_3 = 0;
+let point_player_4 = 0;
+let point_player_1_old = 0;
+let point_player_2_old = 0;
+let point_player_3_old = 0;
+let point_player_4_old = 0;
+document.getElementById("calculator").addEventListener("click", myFunction);
+
+function myFunction() {
+  point_player_1_old = point_player_1;
+  point_player_2_old = point_player_2;
+  point_player_3_old = point_player_3;
+  point_player_4_old = point_player_4;
+
+  point_player_1 += Number(document.querySelector("input[name=point_1]").value);
+  point_player_2 += Number(document.querySelector("input[name=point_2]").value);
+  point_player_3 += Number(document.querySelector("input[name=point_3]").value);
+  point_player_4 += Number(document.querySelector("input[name=point_4]").value);
+
+  document.querySelector("input[name=point_player_1]").value = point_player_1;
+  document.querySelector("input[name=point_player_2]").value = point_player_2;
+  document.querySelector("input[name=point_player_3]").value = point_player_3;
+  document.querySelector("input[name=point_player_4]").value = point_player_4;
+  console.log(
+    point_player_1_old,
+    point_player_2_old,
+    point_player_3_old,
+    point_player_4_old
+  );
+  console.log(point_player_1, point_player_2, point_player_3, point_player_4);
+}
+
+document.getElementById("back").addEventListener("click", backToPrev);
+
+function backToPrev() {
+  console.log(
+    point_player_1_old,
+    point_player_2_old,
+    point_player_3_old,
+    point_player_4_old
+  );
+  document.querySelector("input[name=point_player_1]").value = point_player_1_old;
+  document.querySelector("input[name=point_player_2]").value = point_player_2_old;
+  document.querySelector("input[name=point_player_3]").value = point_player_3_old;
+  document.querySelector("input[name=point_player_4]").value = point_player_4_old;
+}
+
+document.getElementById("backToNow").addEventListener("click", backToNow);
+
+function backToNow() {
+  console.log(
+    point_player_1,
+    point_player_2,
+    point_player_3,
+    point_player_4
+  );
+  document.querySelector("input[name=point_player_1]").value = point_player_1;
+  document.querySelector("input[name=point_player_2]").value = point_player_2;
+  document.querySelector("input[name=point_player_3]").value = point_player_3;
+  document.querySelector("input[name=point_player_4]").value = point_player_4;
+}

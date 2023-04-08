75d092df444ae445d782a1a8e35eaa83cbf61f01
Thang <mrthang1998142@gmail.com>
2023-04-08 20:02:29 +0700

    update css

--- index.js
+++ index.js
@@ -23,41 +23,50 @@ function myFunction() {
   document.querySelector("input[name=point_player_2]").value = point_player_2;
   document.querySelector("input[name=point_player_3]").value = point_player_3;
   document.querySelector("input[name=point_player_4]").value = point_player_4;
-  console.log(
-    point_player_1_old,
-    point_player_2_old,
-    point_player_3_old,
-    point_player_4_old
-  );
-  console.log(point_player_1, point_player_2, point_player_3, point_player_4);
+
+  document.querySelector("input[name=point_1]").value = "";
+  document.querySelector("input[name=point_2]").value = "";
+  document.querySelector("input[name=point_3]").value = "";
+  document.querySelector("input[name=point_4]").value = "";
 }
 
 document.getElementById("back").addEventListener("click", backToPrev);
 
 function backToPrev() {
+  document.querySelector("input[name=point_player_1]").value =
+    point_player_1_old;
+  document.querySelector("input[name=point_player_2]").value =
+    point_player_2_old;
+  document.querySelector("input[name=point_player_3]").value =
+    point_player_3_old;
+  document.querySelector("input[name=point_player_4]").value =
+    point_player_4_old;
+  // swap value point now and point old when you back to prev
+  [point_player_1, point_player_1_old] = [point_player_1_old, point_player_1];
+  [point_player_2, point_player_2_old] = [point_player_2_old, point_player_2];
+  [point_player_3, point_player_3_old] = [point_player_3_old, point_player_3];
+  [point_player_4, point_player_4_old] = [point_player_4_old, point_player_4];
+  console.log(point_player_1, point_player_2, point_player_3, point_player_4);
   console.log(
     point_player_1_old,
     point_player_2_old,
     point_player_3_old,
     point_player_4_old
   );
-  document.querySelector("input[name=point_player_1]").value = point_player_1_old;
-  document.querySelector("input[name=point_player_2]").value = point_player_2_old;
-  document.querySelector("input[name=point_player_3]").value = point_player_3_old;
-  document.querySelector("input[name=point_player_4]").value = point_player_4_old;
 }
 
-document.getElementById("backToNow").addEventListener("click", backToNow);
+document.getElementById("luckyNumber").addEventListener("click", luckyNumber);
 
-function backToNow() {
-  console.log(
-    point_player_1,
-    point_player_2,
-    point_player_3,
-    point_player_4
-  );
-  document.querySelector("input[name=point_player_1]").value = point_player_1;
-  document.querySelector("input[name=point_player_2]").value = point_player_2;
-  document.querySelector("input[name=point_player_3]").value = point_player_3;
-  document.querySelector("input[name=point_player_4]").value = point_player_4;
+function luckyNumber() {
+  if (
+    document.getElementsByClassName("lucky-number")[0].style.display === "block"
+  ) {
+    document.getElementsByClassName("lucky-number")[0].style.display = "none";
+  } else {
+    document.getElementsByClassName("lucky-number")[0].style.display = "block";
+  }
 }
+
+const changeName = (e) => {
+  document.getElementById("playerList").options[0].text = e.value;
+};
